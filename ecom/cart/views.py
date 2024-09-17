2a42861beaf20d4f285b19c19d6af3ce7726190c
Lazizbek0001 <normatovlaziz773@gmail.com>
2024-09-17 14:37:08 +0500

    notifications added

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
-
+from django.contrib import messages
 # Create your views here.
 
 def cart_summary(request):
@@ -24,6 +24,7 @@ def cart_add(request):
         cart_quantity = cart.__len__()
         # response = JsonResponse({'Product Name':product.name})
         response = JsonResponse({'qty':cart_quantity,})
+        messages.success(request,("Product added to the cart!!!"))
         return response
     pass
 def cart_delete(request):
@@ -32,6 +33,7 @@ def cart_delete(request):
     cart.delete(product = product_id)
     
     response = JsonResponse({'product':product_id})
+    messages.success(request,("Product has been removed form the cart!!!"))
     return response
     
 def cart_update(request):
@@ -44,6 +46,7 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
         
         response = JsonResponse({'qty':product_qty})
+        messages.success(request,("Product quantity has been updated!!!"))
         return response
     
 
