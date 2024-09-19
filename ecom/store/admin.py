10cdbafc9fee7ac5321dcc6c08b42e9a686ba13a
Lazizbek Normatov <147187132+Lazizbek0001@users.noreply.github.com>
2024-09-19 21:40:27 +0500

    Add files via upload

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Category, Customer, Order, Product
-
+from .models import Category, Customer, Order, Product, Profile
+from django.contrib.auth.models import User
 
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Product)
+admin.site.register(Profile)
 
-# Register your models here.
+# Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+    
+    
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
