62f14695b65656b938e0b87d807e055eda50e22b
Lazizbek0001 <normatovlaziz773@gmail.com>
2024-09-20 12:06:38 +0500

    cart part1

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,7 +1,8 @@
-from store.models import Product
+from store.models import Product, Profile
 class Cart():
     def __init__(self, request):
         self.session = request.session
+        self.request = request
         
         cart = self.session.get('session_key')
         if 'session_key' not in request.session:
@@ -18,6 +19,12 @@ class Cart():
             self.cart[product_id]=int(product_qty)
             
         self.session.modified = True
+        if self.request.user.is_authenticated:
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+            
+            current_user.update(old_cart=str(carty))
         
     def __len__(self):
         return len(self.cart)
