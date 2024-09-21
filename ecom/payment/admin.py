6936c845f8948549fdca96dc6b3f087a91bc63ff
Lazizbek0001 <normatovlaziz773@gmail.com>
2024-09-21 21:36:36 +0500

    Customer order

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 
 
 admin.site.register(ShippingAddress)
-# Register your models here.
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from store.models import Product
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -18,3 +18,32 @@ class ShippingAddress(models.Model):
         
     def __str__(self) -> str:
         return f"Shipping Address - {str(self.id)}"
+    
+    
+# Create order model
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=1500)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+
+
+#create order item model
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return f'Order Item - {str(self.id)}'
