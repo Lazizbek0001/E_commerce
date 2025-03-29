7f364fbb19c273d32c488cba61fae4d1b6d59b9a
Lazizbek <normatovlaziz9@gmail.com>
2025-03-29 13:50:30 +0500

    seller_side deleted

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store','cart','payment',
-    'seller_side',
     'whitenoise.runserver_nostatic',
 ]
 
--- ecom/seller_side/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-import datetime
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-
-class Seller(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-# Create your models here.
